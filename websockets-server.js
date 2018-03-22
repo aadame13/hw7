fccd1a5ee7a5474eb1dfd17ac720c41e2a1f91a2
Alan Adame <aadame4@csu.fullerton.edu>
2018-03-21 19:48:28 -0700

    added chat topics functionality

--- websockets-server.js
+++ websockets-server.js
@@ -5,18 +5,31 @@ var ws = new WebSocketServer({
   port: port
 });
 var messages = [];
+var tPrefix = "*** Topic is ";
+var tMsg = "";
+var topic = "";
 
 console.log("websockets server started");
 
 ws.on("connection", function(socket) {
   console.log("client connection established");
 
+  if ("" !== topic) socket.send(topic);
   messages.forEach(function(msg) {
     socket.send(msg);
   });
 
+
   socket.on("message", function(data) {
     console.log("message received: " + data);
+
+
+    if (0 === data.indexOf("/topic")) {
+      tMsg = "\"" + data.replace("/topic", "").trim() + "\"";
+      data = "*** Topic has changed to " + tMsg;
+      topic = tPrefix + tMsg;
+    }
+
     messages.push(data);
     ws.clients.forEach(function(clientSocket) {
       clientSocket.send(data)
